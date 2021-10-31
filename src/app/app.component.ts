341e74d1fffbbb41fed4ea12a0c4b17a2c8640c6
irwinet <irwinestradatorres@gmail.com>
2021-10-31 16:14:52 -0500

    Implement Interceptor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UsuariosService } from './services/usuarios.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'interceptorApp';
+
+  constructor(private usuariosService: UsuariosService) {
+    this.usuariosService.obtenerUsuarios()
+      .subscribe(res => {
+        console.log(res);
+      }, (err)=> {
+        console.log('Error en el app component');
+      });
+  }
+
 }
---
+++ src/app/services/usuarios.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+
+import { catchError, map } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosService {
+
+  constructor(private http: HttpClient) { }
+
+  obtenerUsuarios() {
+
+    let params = new HttpParams().append('page', '2');
+    params = params.append('nombre', 'Irwin');
+
+    // const headers = new HttpHeaders({
+    //   'token-usuario': 'ABC12345'
+    // });
+
+    return this.http.get(`https://reqres.in/api/user`, {
+      params
+      // headers
+    }).pipe(
+      map((resp: any) => resp['data']),
+      catchError(err => this.manejarError)
+    );
+  }
+
+  manejarError(error: HttpErrorResponse) {
+    console.log('Sucedio un error');
+    console.log('Registrado en el log file');
+    console.warn(error);
+    return throwError('Error personalizado');
+  }
+
+}
